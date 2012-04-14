48b74203ed744bb5fba0820644eb47035588ea19
Narendran Thangarajan <narendran.thangarajan@gmail.com>
2012-04-14 11:16:06 +0530

    Initilize Peer

--- PeerClient.py
+++ PeerClient.py
@@ -1,11 +1,21 @@
+import socket
 from Constants import *
+
 class PeerClient:
 	def __init__(self):
-		self.__initialize();
+		self.__initialize()
 		
 
 	def __initialize(self):
-		self.maxpeers = MAX_PEERS;
+		self.maxpeers = MAX_PEERS
+		self.peerId = 1001 #TODO : Should think of a mechanism to make it gloally unique
+		self.IPAddr = self.getIp();
+
+	def getIp(self):
+		s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+		s.connect(("google.com",80))
+		return s.getsockname()[0]
+
 
 
 peerClient = PeerClient()
